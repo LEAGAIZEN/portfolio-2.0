3ab7cd4aa5b6f86951127fae856d2bb9cc96987d
leagizen <dark13242@gmail.com>
2023-11-13 17:02:47 +0530

    changes

--- script.js
+++ script.js
@@ -4,23 +4,38 @@
 
 
 // // // /*========== scroll sections active link ==========*/
-// // let sections = document.querySelectorAll('section');
-// // let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-// window.onscroll = () => {
+window.onscroll = () => {
 
-// let header=document.querySelector('.header');
-// header.classList.toggle('sticky' , window.scrollY > 1);
+let header=document.querySelector('.header');
+header.classList.toggle('sticky' , window.scrollY > 1);
 
 
 //     /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
 
-// };
+};
 
-
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 
 // /*========== dark light mode ==========*/
-
-
+let darkModeIcon = document.querySelector('#darkMode-icon');
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle('bx-sun');
+  document.body.classList.toggle('dark-mode');
+};
 // /*========== scroll reveal ==========*/
